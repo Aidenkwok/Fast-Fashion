236d1dfd7bdd138b056f749295dcf9180ab738ed
Aidenkwok <aidenkwok96@gmail.com>
2017-02-14 14:33:00 +0000

    New Facts

--- js/app.js
+++ js/app.js
@@ -2,6 +2,3 @@ $(document).foundation()
 
 $('#randomBackground').css({'background-image': 'url('+ images[Math.floor(Math.random() * images.length)] + ') '});
 $('#randomFact').text(facts[Math.floor(Math.random() * facts.length)])
-
-
-//$('#randomBackground').css({'background-image: url('+ (images[Math.floor(Math.random() * images.length)] + ')'})
