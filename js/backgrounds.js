252a3d5bd46221b99566768ef55478ff9fdb7dcf
Aidenkwok <aidenkwok96@gmail.com>
2017-02-14 14:41:18 +0000

    styling changes

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,5 +1,5 @@
 var images = [
-'https://i.ytimg.com/vi/v1SabYdIlZI/maxresdefault.jpg',
+'http://philanthrobeats.org.uk/wp-content/uploads/2015/12/fast-fashion-2.jpg',
 'http://www.planwallpaper.com/static/images/6942095-abstract-background-wallpaper.jpg',
 'http://media.istockphoto.com/photos/night-scene-milky-way-background-picture-id521011652?k=6&m=521011652&s=170667a&w=0&h=ub3diRak2cAz6sYcnynNCMZmJJXtkYnBR4qNBr7jj4M=',
 'http://www.planwallpaper.com/static/images/518164-backgrounds.jpg',
