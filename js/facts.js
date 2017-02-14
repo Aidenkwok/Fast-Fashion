e9c4d946f93e51ade274a2455dbdcd9991b77191
Aidenkwok <aidenkwok96@gmail.com>
2017-02-14 20:39:12 +0000

    corrections

--- js/facts.js
+++ js/facts.js
@@ -1,10 +1,10 @@
 var facts = [
 "Felmale workers get beaten up for asking for better conditions",
-"1 in 6 people alive in the world today work in some part fo the global fashion industry",
+"1 in 6 people alive in the world today work in some part of the global fashion industry",
 "in the US, the textile industry is the 5th largest contributor to the nation's overall co2 emissions",
 "in Texas, 80% of cotton growing acres is now GMO",
 "textiles account for 19.8 tons of the average american's annual greenhouse emissions",
-"one tshirt has a carbon footprint of 6 kilos- 20 itmes its own weight!",
+"one tshirt has a carbon footprint of 6 kilos- 20 times its own weight!",
 "the textile industry emits toxic chemicals such as formaldehyde and acetic acid into the air; as well as emitting oil mists, solvent vapours, dust and lint, which are associated with many respiratory illnesses.",
 "cotton is the most pesticide intensive crop in the world, and takes up large proportions of agricultural land needed by local peoples to grow their own food",
 "growing cotton accounts for 22.5% of global insecticide use",
