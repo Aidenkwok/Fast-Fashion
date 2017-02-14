911d7dfd34fa0a99eae1e433b84fa5904aab6b42
Aidenkwok <aidenkwok96@gmail.com>
2017-02-14 14:52:45 +0000

    added gifs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).foundation()
 
 $(document).ready(function(){
-    $("#randomFact").hide(0).delay(500).fadeIn(1000)
+    $("#randomFact").hide(0).delay(500).fadeIn(800)
 });
 
-$('#randomBackground').css({'background-image': 'url('+ images[Math.floor(Math.random() * images.length)] + ') '});
+$('#randomBackground').css({'background-image': 'url(./img/'+ images[Math.floor(Math.random() * images.length)] + ') '});
 $('#randomFact').text(facts[Math.floor(Math.random() * facts.length)]);
--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,6 +1,3 @@
 var images = [
-'http://philanthrobeats.org.uk/wp-content/uploads/2015/12/fast-fashion-2.jpg',
-'http://www.planwallpaper.com/static/images/6942095-abstract-background-wallpaper.jpg',
-'http://media.istockphoto.com/photos/night-scene-milky-way-background-picture-id521011652?k=6&m=521011652&s=170667a&w=0&h=ub3diRak2cAz6sYcnynNCMZmJJXtkYnBR4qNBr7jj4M=',
-'http://www.planwallpaper.com/static/images/518164-backgrounds.jpg',
+"Disaster.gif",
 ];
